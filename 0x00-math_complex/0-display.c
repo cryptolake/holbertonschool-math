9213212b21689d1ef4241b2bc54858c422f7f7ab
cryptolake <dhiadah@gmail.com>
2021-11-19 15:19:34 +0100

    fixed!

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -3,7 +3,28 @@
 void display_complex_number(complex c)
 {
 	printf("%.0f", c.re);
-	if (c.im)
-		printf(" + %.0fi", c.im);
+
+	if (c.im == 1 || c.im == -1)
+	{
+		if (c.im < 0)
+			printf(" - ");
+		else
+			printf(" + ");
+		printf("i");
+
+	}
+
+	else if (c.im)
+	{
+		if (c.im < 0)
+		{
+			printf(" - ");
+			c.im *= -1;
+		}
+		else
+			printf(" + ");
+
+		printf("%.0fi", c.im);
+	}
 	printf("\n");
 }
