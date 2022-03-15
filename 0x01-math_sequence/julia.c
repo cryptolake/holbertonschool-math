1f50b2c800c9f3c33cc8cb42922b2cf16cd98bbd
cryptolake <dhiadah@gmail.com>
2022-03-15 11:13:02 +0100

    julia done

--- 0x01-math_sequence/julia.c
+++ 0x01-math_sequence/julia.c
@@ -1,6 +1,11 @@
 #include "mandelbrot.h"
 #define MAXITER 255
 
+double modulus(complex c)
+{
+	return (sqrt(pow(c.im, 2) + pow(c.re, 2)));
+}
+
 
 complex addition(complex c1, complex c2)
 {
@@ -43,16 +48,15 @@ complex plot_pixel(double i, double j, double width,
 	return z;
 }
 
-int mandelbrot(complex c)
+int julia(complex z, complex c)
 {
-	complex f;
 	int i;
 
 	for (i = 0; i < MAXITER; i++)
 	{
-		f = addition(multiplication(f, f), c);
+		z = addition(multiplication(z, z), c);
 
-		if (fabs(f.re) > 2)
+		if (modulus(z) > 2)
 			return i;
 	}
 
@@ -60,29 +64,32 @@ int mandelbrot(complex c)
 
 }
 
-int main(void)
+int main(int argc, char *argv[])
 {
 	FILE *fp;
 	size_t xres, yres, x, y;
-	complex c;
+	complex z, c;
 	double scale;
 	double ypos, xpos;
 
-	/* if (argc != 7) */
-	/* { */
-	/* 	printf("Usage: %s <xres> <yres> <scale> <xpos> <ypos> <filename>\n", argv[0]); */
-	/* 	exit(1); */
-	/* } */
+	if (argc != 9)
+	{
+		printf("Usage: %s <xres> <yres> <scale> <xpos> <ypos> <c.real> <c.im> <filename>\n", argv[0]);
+		exit(1);
+	}
 
-	xres = 1000;
-	yres = 1000;
+	xres = atoi(argv[1]);
+	yres = atoi(argv[2]);
 
-	scale = 4;
+	scale = atof(argv[3]);
 
-	xpos = -0.5;
-	ypos = 0;
+	xpos = atof(argv[4]);
+	ypos = atof(argv[5]);
 
-	fp = fopen("mandelbrot.pgm", "wb");
+	c.re = atof(argv[6]);
+	c.im = atof(argv[7]);
+
+	fp = fopen(argv[8], "wb");
 
 	/* initialize the pgm file */
 	fprintf(fp, "P2\n");
@@ -93,8 +100,8 @@ int main(void)
 	{
 		for (x = 0; x < xres; x++)
 		{
-			c = plot_pixel(x, y, xres, yres, scale, xpos, ypos);
-			fprintf(fp, "%d ", mandelbrot(c));
+			z = plot_pixel(x, y, xres, yres, scale, xpos, ypos);
+			fprintf(fp, "%d ", julia(z, c));
 		}
 		fprintf(fp, "\n");
 	}
